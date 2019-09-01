ab7c03097feae3d0baa59b5f2f2bb05667f1dbd1
Simon Tennant <simon@imaginator.com>
2019-09-01 15:18:40 +0200

    habpanelviewer settings

--- salt/openhab-tablets/files/setup.sh
+++ salt/openhab-tablets/files/setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -xe
+adb install -r -g de.vier_bier.habpanelviewer_8.apk
 adb connect test-wall-tablet:5555 && sleep 1
+adb shell settings put global wifi_sleep_policy 2
+adb shell svc power stayon true
 adb shell pm disable org.cyanogenmod.audiofx
 adb shell pm disable com.cyanogenmod.trebuchet
 adb shell pm clear de.vier_bier.habpanelviewer
@@ -8,4 +11,5 @@ adb shell pm disable de.vier_bier.habpanelviewer
 adb push shared_prefs /data/data/de.vier_bier.habpanelviewer/
 adb shell pm enable de.vier_bier.habpanelviewer
 adb shell am start -n de.vier_bier.habpanelviewer/.MainActivity
+adb shell dpm set-device-owner de.vier_bier.habpanelviewer/.AdminReceiver
 adb disconnect
