2aca20bb44f9f66558c8e9e61b92d90307ae9624
Simon Tennant <simon@imaginator.com>
2021-05-29 07:12:33 +0200

    disable habpanel

--- salt/openhab-server/files/habpanel/habpanel-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-panelsRegistry=$(jq 'tostring' /etc/openhab/habpanel/panelsRegistry.json )
-jq '.panelsRegistry += '"${panelsRegistry}"'  ' < /etc/openhab/habpanel/org.openhab.habpanel.json | curl --silent -X PUT --header "Content-Type: application/json" --header "Accept: application/json"  -d @- "http://127.0.0.1:8080/rest/services/org.openhab.habpanel/config"
-
-openhab-cli console -b -u openhab -p habopen <<_EOF_
-smarthome:send Tablet_Commands 'RESTART'
-_EOF_
