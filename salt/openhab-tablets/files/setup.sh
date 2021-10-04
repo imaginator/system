22c299852e263a6500b08235db529c44332d30ef
Simon Tennant <simon@imaginator.com>
2021-10-04 16:55:31 +0200

    fix table deployment script

--- salt/openhab-tablets/files/setup.sh
+++ salt/openhab-tablets/files/setup.sh
@@ -1,40 +1,46 @@
 #!/bin/bash
-set -x
+#set -x
 
 export room=$1
 
-if [ -n "$room" ]; then
-  echo "Deploying to $room"
-else
-  echo "Missing room: Kitchen/Lounge/Study/HallwayLarge/BathMain/BathGuest/Bedroom1/2/3"
-  exit 0
+if [[ $(echo "$room" | grep -LE 'Kitchen|Lounge|Study|HallwayLarge|BathMain|BathGuest|Bedroom1|Bedroom2|Bedroom3') ]]; then 
+  echo "Missing room parameter incorrectly capitalised e.g Lounge"
+  exit 1
 fi
 
+echo "Deploying to $room"
+
+echo "running ADB non-root commands"
 adb kill-server
-adb connect tablet-$room.imagilan
-sleep 2
-
-# adb shell dpm remove-active-admin de.vier_bier.habpanelviewer/.AdminReceiver  
-adb uninstall de.vier_bier.habpanelviewer
-adb install de.vier_bier.habpanelviewer.apk
-
-# adb shell pm disable    de.vier_bier.habpanelviewer
-
-adb root &&  adb connect tablet-$room.imagilan
-sleep 2
-# needed to write to /data
-
-adb shell rm -r /data/data/de.vier_bier.habpanelviewer/shared_prefs
-adb push shared_prefs /data/data/de.vier_bier.habpanelviewer/
-# adb shell mv /data/data/de.vier_bier.habpanelviewer/shared_prefs/de.vier_bier.habpanelviewer_preferences.xml /data/data/de.vier_bier.habpanelviewer/shared_prefs/de.vier_bier.habpanelviewer_preferences.xml.old
-# envsubst < de.vier_bier.habpanelviewer_preferences.xml | adb exec-in "cat - > /data/data/de.vier_bier.habpanelviewer/shared_prefs/de.vier_bier.habpanelviewer_preferences.xml"
-envsubst < de.vier_bier.habpanelviewer_preferences.xml > /tmp/de.vier_bier.habpanelviewer_preferences.xml 
-adb push /tmp/de.vier_bier.habpanelviewer_preferences.xml /data/data/de.vier_bier.habpanelviewer/shared_prefs/
+adb connect tablet-$room.imagilan:5555 && sleep 2
+adb shell settings put global bluetooth_on 0
+adb shell settings put system accelerometer_rotation 1
+adb shell settings put system user_rotation 1
+adb shell settings put global wifi_sleep_policy 2 # stop wifi from sleeping
+adb shell svc power stayon false
+adb shell setprop persist.sys.timezone Europe/Berlin
+adb shell setprop persist.adb.tcp.port 5555   # keep adb via wifi enabled
+adb shell settings put system screen_auto_brightness_adj 0.8
+adb shell settings put system screen_brightness_mode 1 # auto brightness mode
+adb shell settings put system screen_off_timeout 120000    # milliseconds (needs to be longer than rule)
+adb shell dumpsys deviceidle whitelist +de.vier_bier.habpanelviewer # disable battery optimistaion
 adb shell cmd package set-home-activity de.vier_bier.habpanelviewer/.StartActivity # set as launcher
 adb shell am force-stop de.vier_bier.habpanelviewer #since disable doesn't kill
-adb shell am start -n de.vier_bier.habpanelviewer/.MainActivity
+adb uninstall de.vier_bier.habpanelviewer
+adb install de.vier_bier.habpanelviewer.apk
+adb shell pm grant de.vier_bier.habpanelviewer android.permission.RECORD_AUDIO #stupid dialogs
+adb shell pm grant de.vier_bier.habpanelviewer android.permission.CAMERA
+adb shell cmd package set-home-activity de.vier_bier.habpanelviewer/.StartActivity # set as launcher
+adb shell dumpsys deviceidle whitelist +de.vier_bier.habpanelviewer  # disable battery optimisation
 
-# remove cruft (also only as root)
+echo "running ADB root commands"   # needed to write to /data
+adb root &&  adb connect tablet-$room.imagilan:5555 && sleep 2
+adb shell rm -r /data/data/de.vier_bier.habpanelviewer/shared_prefs
+adb shell mkdir -m 777 -p /data/data/de.vier_bier.habpanelviewer/shared_prefs
+envsubst < de.vier_bier.habpanelviewer_preferences.xml > /tmp/de.vier_bier.habpanelviewer_preferences.xml 
+adb push /tmp/de.vier_bier.habpanelviewer_preferences.xml /data/data/de.vier_bier.habpanelviewer/shared_prefs/de.vier_bier.habpanelviewer_preferences.xml
+adb push _has_set_default_values.xml                      /data/data/de.vier_bier.habpanelviewer/shared_prefs/_has_set_default_values.xml
+adb shell chmod 777 /data/data/de.vier_bier.habpanelviewer/shared_prefs/*
 adb shell pm disable com.android.calculator2
 adb shell pm disable com.android.calendar
 adb shell pm disable com.android.contacts
@@ -53,29 +59,5 @@ adb shell pm disable com.cyanogenmod.setupwizard
 adb shell pm disable org.cyanogenmod.audiofx
 adb shell pm disable org.cyanogenmod.snap
 adb shell pm disable org.lineageos.recorder
-
-
-adb unroot && adb connect tablet-$room.imagilan
-sleep 2
-
-adb de.vier_bier.habpanelviewer/.StartActivity
-
-adb shell settings put global bluetooth_on 0
-adb shell settings put system accelerometer_rotation 1
-adb shell settings put system user_rotation 1
-adb shell settings put global wifi_sleep_policy 2 # stop wifi from sleeping
-adb shell svc power stayon false
-adb shell setprop persist.sys.timezone Europe/Berlin
-adb shell setprop persist.adb.tcp.port 5555   # keep adb via wifi enabled
-
-# disable battery optimistaion
-adb shell dumpsys deviceidle whitelist +de.vier_bier.habpanelviewer 
-# set device admin permission
-# adb shell dpm set-active-admin current de.vier_bier.habpanelviewer/.AdminReceiver  
-
-
-
-
-
-#adb reboot
-adb disconnect
+adb reboot
+adb disconnect tablet-$room.imagilan
