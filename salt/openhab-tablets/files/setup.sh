a1648e46e890177816876480e4845b6273a1a94b
Simon Tennant <simon@imaginator.com>
2021-10-10 15:04:01 +0200

    ensure habpanelviewer prefs are correctly set

--- salt/openhab-tablets/files/setup.sh
+++ salt/openhab-tablets/files/setup.sh
@@ -3,7 +3,7 @@
 
 export room=$1
 
-if [[ $(echo "$room" | grep -LE 'Kitchen|Lounge|Study|HallwayLarge|BathMain|BathGuest|Bedroom1|Bedroom2|Bedroom3') ]]; then 
+if [[ $(echo "$room" | grep -LE 'Kitchen|Lounge|Study|HallwayLarge|BathroomMain|BathroomGuest|Bedroom1|Bedroom2|Bedroom3') ]]; then 
   echo "Missing room parameter incorrectly capitalised e.g Lounge"
   exit 1
 fi
@@ -23,8 +23,6 @@ adb shell setprop persist.adb.tcp.port 5555   # keep adb via wifi enabled
 adb shell settings put system screen_auto_brightness_adj 0.8
 adb shell settings put system screen_brightness_mode 1 # auto brightness mode
 adb shell settings put system screen_off_timeout 120000    # milliseconds (needs to be longer than rule)
-adb shell dumpsys deviceidle whitelist +de.vier_bier.habpanelviewer # disable battery optimistaion
-adb shell cmd package set-home-activity de.vier_bier.habpanelviewer/.StartActivity # set as launcher
 adb shell am force-stop de.vier_bier.habpanelviewer #since disable doesn't kill
 adb uninstall de.vier_bier.habpanelviewer
 adb install de.vier_bier.habpanelviewer.apk
@@ -33,14 +31,17 @@ adb shell pm grant de.vier_bier.habpanelviewer android.permission.CAMERA
 adb shell cmd package set-home-activity de.vier_bier.habpanelviewer/.StartActivity # set as launcher
 adb shell dumpsys deviceidle whitelist +de.vier_bier.habpanelviewer  # disable battery optimisation
 
+# config files
+adb shell run-as de.vier_bier.habpanelviewer mkdir -p    /data/data/de.vier_bier.habpanelviewer/shared_prefs
+envsubst < de.vier_bier.habpanelviewer_preferences.xml > /tmp/de.vier_bier.habpanelviewer_preferences.xml
+adb push /tmp/de.vier_bier.habpanelviewer_preferences.xml /sdcard/de.vier_bier.habpanelviewer_preferences.xml
+adb push _has_set_default_values.xml                      /sdcard/_has_set_default_values.xml
+adb shell run-as de.vier_bier.habpanelviewer  cp /sdcard/_has_set_default_values.xml  /data/data/de.vier_bier.habpanelviewer/shared_prefs/_has_set_default_values.xml
+adb shell run-as de.vier_bier.habpanelviewer  cp /sdcard/de.vier_bier.habpanelviewer_preferences.xml /data/data/de.vier_bier.habpanelviewer/shared_prefs/de.vier_bier.habpanelviewer_preferences.xml
+
+
 echo "running ADB root commands"   # needed to write to /data
 adb root &&  adb connect tablet-$room.imagilan:5555 && sleep 2
-adb shell rm -r /data/data/de.vier_bier.habpanelviewer/shared_prefs
-adb shell mkdir -m 777 -p /data/data/de.vier_bier.habpanelviewer/shared_prefs
-envsubst < de.vier_bier.habpanelviewer_preferences.xml > /tmp/de.vier_bier.habpanelviewer_preferences.xml 
-adb push /tmp/de.vier_bier.habpanelviewer_preferences.xml /data/data/de.vier_bier.habpanelviewer/shared_prefs/de.vier_bier.habpanelviewer_preferences.xml
-adb push _has_set_default_values.xml                      /data/data/de.vier_bier.habpanelviewer/shared_prefs/_has_set_default_values.xml
-adb shell chmod 777 /data/data/de.vier_bier.habpanelviewer/shared_prefs/*
 adb shell pm disable com.android.calculator2
 adb shell pm disable com.android.calendar
 adb shell pm disable com.android.contacts
