5d7d4284b41a4e50da8c29cfd31201c8be161e7e
Simon Tennant <simon@imaginator.com>
2022-06-08 18:50:00 +0200

    Upgrading tablets

--- salt/openhab-tablets/files/setup.sh
+++ salt/openhab-tablets/files/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#set -x
+set -x
 
 export room=$1
 
@@ -17,35 +17,33 @@ fi
 echo "running ADB non-root commands"
 adb kill-server
 adb connect tablet-$room.imagilan:5555 && sleep 2
-adb shell settings put global bluetooth_on 0
-adb shell settings put secure lockscreen_disabled 1
-adb shell settings put system accelerometer_rotation 1
-adb shell settings put system user_rotation 1
-adb shell settings put global wifi_sleep_policy 2                                           # stop wifi from sleeping
-adb shell svc power stayon false
-adb shell setprop persist.sys.timezone Europe/Berlin
+adb shell settings put system volume_system 0
 adb shell setprop persist.adb.tcp.port 5555                                                 # keep adb via wifi enabled
-adb shell settings put system screen_auto_brightness_adj 0.8
+adb shell setprop persist.sys.timezone Europe/Berlin
+adb shell settings put global bluetooth_on 0
+adb shell settings put global wifi_on 1
+adb shell settings put system accelerometer_rotation 1
 adb shell settings put system screen_brightness_mode 1                                      # auto brightness mode
-adb shell settings put system screen_off_timeout 120000                                     # milliseconds (now shut off here rather than rule)
+adb shell settings put system screen_off_timeout 300000                                     # milliseconds (now shut off here rather than rule)
+adb shell settings put system user_rotation 1
+adb shell svc power stayon false
+adb shell svc wifi enable
+
 adb shell am force-stop org.openhab.habdroid                                                #since disable doesn't kill
-adb uninstall org.openhab.habdroid
-adb install -r /tmp/org.openhab.habdroid.apk
-#adb shell pm grant de.vier_bier.habpanelviewer android.permission.RECORD_AUDIO              #stupid dialogs
-#adb shell pm grant de.vier_bier.habpanelviewer android.permission.CAMERA
-#adb shell cmd package set-home-activity de.vier_bier.habpanelviewer/.StartActivity          # set as launcher
+adb uninstall           org.openhab.habdroid
+adb   install -r   /tmp/org.openhab.habdroid.apk
 adb shell dumpsys deviceidle whitelist +org.openhab.habdroid                                 # disable battery optimisation
 
 echo "running ADB root commands"   # needed to write to /data
 adb root &&  adb connect tablet-$room.imagilan:5555 && sleep 2
 
 # Disable lock screen
-adb shell /system/bin/sqlite3 /data/system/locksettings.db "UPDATE locksettings SET value = '1' WHERE name = 'lockscreen.disabled'"
-adb shell /system/bin/sqlite3 /data/system/locksettings.db "UPDATE locksettings SET value = '0' WHERE name = 'lockscreen.password_type'"
-adb shell /system/bin/sqlite3 /data/system/locksettings.db "UPDATE locksettings SET value = '0' WHERE name = 'lockscreen.password_type_alternate'"
+adb shell /system/bin/sqlite3 /data/system/locksettings.db \"UPDATE locksettings SET value = \'1\' WHERE name = \'lockscreen.disabled\'\"
+adb shell /system/bin/sqlite3 /data/system/locksettings.db \"UPDATE locksettings SET value = \'0\' WHERE name = \'lockscreen.password_type\'\"
+adb shell /system/bin/sqlite3 /data/system/locksettings.db \"UPDATE locksettings SET value = \'0\' WHERE name = \'lockscreen.password_type_alternate\'\"
 
 # add prefs
-adb shell mkdir -p                             /data/data/org.openhab.habdroid/shared_prefs
+adb shell mkdir -m 777 -p                      /data/data/org.openhab.habdroid/shared_prefs
 adb shell chmod -R 777                         /data/data/org.openhab.habdroid/shared_prefs
 adb push  org.openhab.habdroid_preferences.xml /data/data/org.openhab.habdroid/shared_prefs/org.openhab.habdroid_preferences.xml
 
@@ -62,16 +60,14 @@ adb shell pm disable com.android.managedprovisioning
 adb shell pm disable com.android.onetimeinitializer
 adb shell pm disable com.android.providers.calendar
 adb shell pm disable com.android.smspush
-adb shell pm disable com.cyanogenmod.eleven
-adb shell pm disable com.cyanogenmod.lockclock
-adb shell pm disable com.cyanogenmod.setupwizard
-adb shell pm disable org.cyanogenmod.audiofx
-adb shell pm disable org.cyanogenmod.snap
+adb shell pm disable org.lineageos.eleven
+adb shell pm disable org.lineageos.lockclock
+adb shell pm disable org.lineageos.setupwizard
+adb shell pm disable org.lineageos.audiofx
 adb shell pm disable org.lineageos.recorder
+
 
 adb reboot
 adb disconnect tablet-$room.imagilan
 
-# Notes 
-
-# adb shell dpm set-device-owner com.santoshdevadiga.kiosksample/.DeviceAdminReceiver.
+#adb shell am start -n org.openhab.habdroid/.ui.MainActivity
