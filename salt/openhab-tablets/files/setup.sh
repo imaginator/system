a4644ed41a80d361578660f304dd20a753159d84
Simon Tennant <simon@imaginator.com>
2022-06-08 14:40:09 +0200

    switch from Habpanel to Official Android App

--- salt/openhab-tablets/files/setup.sh
+++ salt/openhab-tablets/files/setup.sh
@@ -10,38 +10,46 @@ fi
 
 echo "Deploying to $room"
 
+if [[ ! -f /tmp/org.openhab.habdroid.apk ]] ; then
+  curl -o /tmp/org.openhab.habdroid.apk  https://f-droid.org/repo/org.openhab.habdroid_418.apk
+fi
+
 echo "running ADB non-root commands"
 adb kill-server
 adb connect tablet-$room.imagilan:5555 && sleep 2
 adb shell settings put global bluetooth_on 0
+adb shell settings put secure lockscreen_disabled 1
 adb shell settings put system accelerometer_rotation 1
 adb shell settings put system user_rotation 1
-adb shell settings put global wifi_sleep_policy 2 # stop wifi from sleeping
+adb shell settings put global wifi_sleep_policy 2                                           # stop wifi from sleeping
 adb shell svc power stayon false
 adb shell setprop persist.sys.timezone Europe/Berlin
-adb shell setprop persist.adb.tcp.port 5555   # keep adb via wifi enabled
+adb shell setprop persist.adb.tcp.port 5555                                                 # keep adb via wifi enabled
 adb shell settings put system screen_auto_brightness_adj 0.8
-adb shell settings put system screen_brightness_mode 1 # auto brightness mode
-adb shell settings put system screen_off_timeout 120000    # milliseconds (needs to be longer than rule)
-adb shell am force-stop de.vier_bier.habpanelviewer #since disable doesn't kill
-adb uninstall de.vier_bier.habpanelviewer
-adb install de.vier_bier.habpanelviewer.apk
-adb shell pm grant de.vier_bier.habpanelviewer android.permission.RECORD_AUDIO #stupid dialogs
-adb shell pm grant de.vier_bier.habpanelviewer android.permission.CAMERA
-adb shell cmd package set-home-activity de.vier_bier.habpanelviewer/.StartActivity # set as launcher
-adb shell dumpsys deviceidle whitelist +de.vier_bier.habpanelviewer  # disable battery optimisation
-
-# config files
-adb shell run-as de.vier_bier.habpanelviewer mkdir -p    /data/data/de.vier_bier.habpanelviewer/shared_prefs
-envsubst < de.vier_bier.habpanelviewer_preferences.xml > /tmp/de.vier_bier.habpanelviewer_preferences.xml
-adb push /tmp/de.vier_bier.habpanelviewer_preferences.xml /sdcard/de.vier_bier.habpanelviewer_preferences.xml
-adb push _has_set_default_values.xml                      /sdcard/_has_set_default_values.xml
-adb shell run-as de.vier_bier.habpanelviewer  cp /sdcard/_has_set_default_values.xml  /data/data/de.vier_bier.habpanelviewer/shared_prefs/_has_set_default_values.xml
-adb shell run-as de.vier_bier.habpanelviewer  cp /sdcard/de.vier_bier.habpanelviewer_preferences.xml /data/data/de.vier_bier.habpanelviewer/shared_prefs/de.vier_bier.habpanelviewer_preferences.xml
-
+adb shell settings put system screen_brightness_mode 1                                      # auto brightness mode
+adb shell settings put system screen_off_timeout 120000                                     # milliseconds (now shut off here rather than rule)
+adb shell am force-stop org.openhab.habdroid                                                #since disable doesn't kill
+adb uninstall org.openhab.habdroid
+adb install -r /tmp/org.openhab.habdroid.apk
+#adb shell pm grant de.vier_bier.habpanelviewer android.permission.RECORD_AUDIO              #stupid dialogs
+#adb shell pm grant de.vier_bier.habpanelviewer android.permission.CAMERA
+#adb shell cmd package set-home-activity de.vier_bier.habpanelviewer/.StartActivity          # set as launcher
+adb shell dumpsys deviceidle whitelist +org.openhab.habdroid                                 # disable battery optimisation
 
 echo "running ADB root commands"   # needed to write to /data
 adb root &&  adb connect tablet-$room.imagilan:5555 && sleep 2
+
+# Disable lock screen
+adb shell /system/bin/sqlite3 /data/system/locksettings.db "UPDATE locksettings SET value = '1' WHERE name = 'lockscreen.disabled'"
+adb shell /system/bin/sqlite3 /data/system/locksettings.db "UPDATE locksettings SET value = '0' WHERE name = 'lockscreen.password_type'"
+adb shell /system/bin/sqlite3 /data/system/locksettings.db "UPDATE locksettings SET value = '0' WHERE name = 'lockscreen.password_type_alternate'"
+
+# add prefs
+adb shell mkdir -p                             /data/data/org.openhab.habdroid/shared_prefs
+adb shell chmod -R 777                         /data/data/org.openhab.habdroid/shared_prefs
+adb push  org.openhab.habdroid_preferences.xml /data/data/org.openhab.habdroid/shared_prefs/org.openhab.habdroid_preferences.xml
+
+# remove cruft 
 adb shell pm disable com.android.calculator2
 adb shell pm disable com.android.calendar
 adb shell pm disable com.android.contacts
@@ -60,5 +68,10 @@ adb shell pm disable com.cyanogenmod.setupwizard
 adb shell pm disable org.cyanogenmod.audiofx
 adb shell pm disable org.cyanogenmod.snap
 adb shell pm disable org.lineageos.recorder
+
 adb reboot
 adb disconnect tablet-$room.imagilan
+
+# Notes 
+
+# adb shell dpm set-device-owner com.santoshdevadiga.kiosksample/.DeviceAdminReceiver.
